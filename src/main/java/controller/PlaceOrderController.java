e80bb7445f2e4aa37e0584ed137e526a0f2d636f
Minh Tuấn Bùi <buiminhtuan1507@gmail.com>
2021-03-19 16:25:39 +0700

    SOLID by bui tuan

--- src/main/java/controller/PlaceOrderController.java
+++ src/main/java/controller/PlaceOrderController.java
@@ -21,6 +21,7 @@ import java.util.regex.Pattern;
  * This class controls the flow of place order usecase in our AIMS project
  * @author nguyenlm
  */
+//SOLID: vi pham nguyen tac SRP  vi 1 lop PlaceOrderController co 2 nhiem vu : 1 la Order , 2 la Validate cac thanh phan cua Order
 public class PlaceOrderController extends BaseController {
 
     /**
@@ -88,6 +89,7 @@ public class PlaceOrderController extends BaseController {
    * @throws InterruptedException
    * @throws IOException
    */
+    //SOLID: OCP vi sau nay neu info thay doi va so luong du lieu can validator thay doi thi phan code nay cung phai thay doi theo
     //(Bui Minh Tuan) minh nghi cho nay là Data coupling vi 3 bien trong ham if thi khac nhau va cac gia tri cua no co the kiem soat duoc
     //Control coupling do info lÃ  tham sá»‘ vá»� máº·t control
     //Tá»©c lÃ  nÃ³ Ä‘iá»ƒu kiá»ƒu luá»“ng cá»§a module Ä‘Æ°á»£c gá»�i
